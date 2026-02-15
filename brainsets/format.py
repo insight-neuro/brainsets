70d2b8cd327515ea8431397d6c47badd89c753ef
alexander-brady <bradya@student.ethz.ch>
2026-02-15 13:39:02 +0100

    feat: standardized NeuralData format

---
+++ brainsets/format.py
@@ -0,0 +1,53 @@
+from collections.abc import Callable
+
+from temporaldata import ArrayDict, Data, RegularTimeSeries
+
+from brainsets.descriptions import (
+    BrainsetDescription,
+    DeviceDescription,
+    SessionDescription,
+    SubjectDescription,
+)
+
+from .core import serialize_fn_map
+
+
+class NeuralData(Data):
+    """Standardized format for neural data."""
+
+    brainset: BrainsetDescription
+    """Brainset-level metadata describing the dataset as a whole."""
+    subject: SubjectDescription
+    """Subject-level metadata describing the individual subject."""
+    session: SessionDescription
+    """Session-level metadata describing the recording session."""
+    device: DeviceDescription
+    """Device-level metadata describing the recording device."""
+    data: RegularTimeSeries
+    """Time series data containing the neural recordings."""
+    channels: ArrayDict
+    """ArrayDict containing metadata for each channel in the neural data."""
+
+    def __init__(
+        self,
+        brainset: BrainsetDescription,
+        subject: SubjectDescription,
+        session: SessionDescription,
+        device: DeviceDescription,
+        data: RegularTimeSeries,
+        channels: ArrayDict,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.brainset = brainset
+        self.subject = subject
+        self.session = session
+        self.device = device
+        self.data = data
+        self.channels = channels
+
+    def to_hdf5(
+        self, file, serialize_fn_map: dict[type, Callable] | None = serialize_fn_map
+    ):
+        return super().to_hdf5(file, serialize_fn_map)
